b60f0cdf9b22961b7c6f825715886801cdb63dde
Lerrel Pinto <lerrel.pinto@gmail.com>
2018-06-18 18:42:20 -0700

    WIP

--- continuous_env.py
+++ continuous_env.py
@@ -3,6 +3,31 @@ from gym import ObservationWrapper
 import numpy as np
 from IPython import embed
 
+class ChoiceDist(object):
+
+    def __init__(self, choices, p=None):
+        self.choices = choices
+        self.p = p
+        if self.p == None:
+            self.p = 1./len(choices)*np.ones(len(choices))
+
+    def sample(self):
+        val = np.random.choice(self.choices, p=self.p)
+        if type(val) in [int, np.int, np.int0, np.int8, np.int16, np.int32, np.int64]:
+            pass
+        else:
+            val = val.sample()
+        return val
+
+
+class FixedResetDist(object):
+
+    def __init__(self, val=0):
+        self.fixed_val = val
+
+    def sample(self):
+        return self.fixed_val
+
 class ContinuousWrapper(ObservationWrapper):
 
     def observation(self, observation):
@@ -18,3 +43,19 @@ class ContinuousWrapper(ObservationWrapper):
             return obs
         else:
             return observation
+
+    def reset(self, **kwargs):
+        observation = self.env.reset(**kwargs)
+        if self.reset_dist is None:
+            self.set_reset_dist(FixedResetDist(0))
+        state = self.reset_dist.sample()
+        if self.env.observation_space.contains(state) == True:
+            self.env.env.s = state
+        elif state is not None:
+            raise ValueError
+        else:
+            pass
+        return self.observation(self.env.env.s)
+
+    def set_reset_dist(self, reset_dist):
+        self.reset_dist = reset_dist
--- deep_q_learning.py
+++ deep_q_learning.py
@@ -1,30 +1,36 @@
 '''
-Frozen version of DQN for FrozenLake to work
+DQN for FrozenLake.
+Performance benchmarks:
+    FrozenLake-v0 - 0.74
+    FrozenLakeDet-v0 - 0.97
+    FrozenLake8x8-v0 - 0.66
+    FrozenLake8x8Det-v0 - 0.97
 '''
 import gym
 
 from baselines import deepq
-from continuous_env import ContinuousWrapper
+from continuous_env import ContinuousWrapper, FixedResetDist
 from IPython import embed
 
 
 def main():
-    env_name = 'FrozenLake-v0'
+    env_name = 'FrozenLake8x8Det-v0'
     model_name = '{}_model.pkl'.format(env_name)
     env = gym.make(env_name)
     env = ContinuousWrapper(env)
+    #env.set_reset_dist(FixedResetDist(62))
     env.reset();
     #embed()
     # Enabling layer_norm here is import for parameter space noise!
-    model = deepq.models.mlp([64])
+    model = deepq.models.mlp([])
     act = deepq.learn(
         env,
         q_func=model,
         lr=1e-3,
         max_timesteps=100000,
         buffer_size=50000,
-        exploration_fraction=0.2,
-        exploration_final_eps=0.02,
+        exploration_fraction=0.1,
+        exploration_final_eps=0.0,
         print_freq=10,
     )
     print("Saving model to {}".format(model_name))
